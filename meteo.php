c2e351d03ebcb17ba0da29ecd0961b55fd81f2e7
David <Natandav39@gmail.com>
2022-10-26 15:42:36 +0200

    first commit

---
+++ meteo.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Météo
+Plugin URI: 
+Description: En cours de maintenance !
+Version: 0.1
+Author: Natan DAVID
+Author URI: natandavid.fr
+License: GPLv2
+*/
+
+defined('ABSPATH') or die('Oups !');
+
+function MyPluginMenu(){
+    add_menu_page(
+        'Météo', // Page Title
+        'Météo', // Menu Title
+        'manage_options', // Capabiliy
+        plugin_dir_path(__FILE__)  .'includes/page.php'
+    );
+}
+
+add_action('admin_menu','MyPluginMenu');
+
